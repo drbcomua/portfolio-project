8d90a08a638f63c6ecabec2f24ff849ba3eb1e4d
Dmitry RUMYANTSEV <dr@b.com.ua>
2019-06-10 02:19:56 +0300

    blog with slugs

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,31 @@
 from django.db import models
-from django import forms
+from django.utils.text import slugify
+
 
 class Blog(models.Model):
+	tag = models.CharField(max_length=50, default="Development")
 	title = models.CharField(max_length=200)
 	pub_date = models.DateField()
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
+	slug = models.SlugField(
+		verbose_name="Slug",
+		allow_unicode=True,
+		unique=True,
+		default='',
+		editable=False,
+	)
+
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		suffix = ""
+		if len(self.body) > 97:
+			suffix = "..."
+		return self.body[:100] + suffix
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = slugify(self.title)
+		super(Blog, self).save(*args, **kwargs)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+
+def allblogs(request):
+	blogs = Blog.objects
+	return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
+
+def detail(request, blog_title):
+	detailblog = get_object_or_404(Blog, slug=blog_title)
+	return render(request, 'blog/detail.html', {'detailblog': detailblog})
